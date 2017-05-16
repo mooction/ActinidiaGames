8c3bfdc238d6888411e95a9b164e65c571214eb0
FANG <389094993@qq.com>
2017-05-16 10:52:36 +0800

    update rpgcommon.lua

--- res-rpg/lua/rpgcommon.lua
+++ res-rpg/lua/rpgcommon.lua
@@ -8,23 +8,29 @@ Usage:
 ]]
 local rpgcommon = {
 	g_box = nil,
-	g_messagebox = nil,
-
-	black = 0xFF181818,
-	white = 0xFFFFFFFF,
-
-	prepare = function(path_box, path_messagebox)
+	g_msgbox = nil,
+	dx_box = 0,
+	dy_box = 0,
+	dx_msg = 0,
+	dy_msg = 0,
+	
+	prepare = function(path_box, path_msgbox)
 		rpgcommon.g_box = GetImage(path_box)
-		rpgcommon.g_messagebox = GetImage(path_messagebox)
+		rpgcommon.g_msgbox = GetImage(path_msgbox)
+
+		rpgcommon.dx_box = (core.screenwidth - GetWidth(rpgcommon.g_box))//2
+		rpgcommon.dy_box = core.screenheight - GetHeight(rpgcommon.g_box)
+
+		rpgcommon.dx_msg = (core.screenwidth - GetWidth(rpgcommon.g_msgbox))//2
+		rpgcommon.dy_msg = (core.screenheight - GetHeight(rpgcommon.g_msgbox))//2
 	end,
 
 	-- 显示交谈框
 	talk = function(g, g_portrait, id_title, id_text)
-		local h = GetHeight(rpgcommon.g_box)
-		PasteToImage(g, rpgcommon.g_box, 0, canvas_height-h)
-		PasteToImage(g, g_portrait, 10, canvas_height-GetHeight(g_portrait))
-		printer.out(g, 164, canvas_height-110, id_title, 0xff)
-		printer.out(g, 164, canvas_height-80, id_text, 0xff)
+		PasteToImage(g, rpgcommon.g_box, rpgcommon.dx_box, rpgcommon.dy_box)
+		PasteToImage(g, g_portrait, rpgcommon.dx_box, core.screenheight-GetHeight(g_portrait))
+		printer.out(g, rpgcommon.dx_box+160, rpgcommon.dy_box+16, id_title, 0xff)
+		printer.out(g, rpgcommon.dx_box+160, rpgcommon.dy_box+46, id_text, 0xff)
 	end,
 
 	alpha = 255,
@@ -37,10 +43,8 @@ local rpgcommon = {
 	-- 显示消息框，显示完毕返回true，否则false
 	message = function(g, id, fade)
 		if rpgcommon.alpha ~= 45 then
-			local dx = (canvas_width - GetWidth(rpgcommon.g_messagebox))//2
-			local dy = (canvas_height - GetHeight(rpgcommon.g_messagebox))//2
-			AlphaBlend(g, rpgcommon.g_messagebox, dx, dy, math.floor(rpgcommon.alpha))
-			printer.out(g, dx+34, dy+34, id, math.floor(rpgcommon.alpha))
+			AlphaBlend(g, rpgcommon.g_msgbox, rpgcommon.dx_msg, rpgcommon.dy_msg, math.floor(rpgcommon.alpha))
+			printer.out(g, rpgcommon.dx_msg+34, rpgcommon.dy_msg+34, id, math.floor(rpgcommon.alpha))
 			rpgcommon.alpha = rpgcommon.alpha - fade
 			return false
 		else
@@ -50,8 +54,8 @@ local rpgcommon = {
 	end,
 
 	free = function()
-		DeleteImage(g_box)
-		DeleteImage(g_messagebox)
+		DeleteImage(rpgcommon.g_box)
+		DeleteImage(rpgcommon.g_msgbox)
 	end
 }
 return rpgcommon
